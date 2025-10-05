ee68cb1028baba0f7ab62b092e8bceba37444012
maximevincent <maxime.vincent@turing.bio>
2025-10-05 17:38:32 +0000

    Ruff formatted

--- src/turingdb_examples/utils.py
+++ src/turingdb_examples/utils.py
@@ -1,3 +1,6 @@
+import re
+
+
 def create_ID_column(df_):
     df = df_.copy()
     df = df.reset_index()
@@ -7,3 +10,13 @@ def create_ID_column(df_):
     df["Patient ID"] = [(max_len - len(id_)) * "0" + id_ for id_ in df["Patient ID"]]
 
     return df
+
+
+def get_return_statements(query):
+    # Extract everything after RETURN
+    match = re.search(r"RETURN\s+(.+)", query, re.IGNORECASE)
+    if match:
+        return_statement = match.group(1).strip()
+        return return_statement.split(", ")
+    else:
+        print("No RETURN clause found")
