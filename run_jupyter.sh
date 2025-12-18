509f98a3205389426ffd9df21ecf73782916b746
maximevincent <maxime.vincent@turing.bio>
2025-12-18 14:17:07 +0000

    Fix run jupyter

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -2,4 +2,5 @@
 
 PORT=${1:-8889}  # Use $1 if provided, otherwise default to 8889
 
-uv run --with jupyter jupyter lab --no-browser --ServerApp.token='' --ServerApp.allow_remote_access=true --port=$PORT --ip=127.0.0.1 > /tmp/jupyter.log 2>&1
+uv run --with jupyter jupyter lab \
+    --no-browser --ServerApp.token='' --ServerApp.allow_remote_access=true --port=$PORT --ip=127.0.0.1 > /tmp/jupyter.log 2>&1
